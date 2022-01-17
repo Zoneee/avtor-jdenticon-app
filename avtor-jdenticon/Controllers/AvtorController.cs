e7b4166ec5eef6e3b16876fd0c2f72c18b23844e
卜星豪 <xinghao.bu@deepbiogroup.com>
2022-01-17 17:19:21 +0800

    first commit

---
+++ avtor-jdenticon/Controllers/AvtorController.cs
@@ -0,0 +1,26 @@
+﻿using Jdenticon;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace avtor_jdenticon.Controllers
+{
+    [ApiController]
+    [Route("avtor")]
+    public class IdenticonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([Required] string value, int size = 100)
+        {
+            var identicon = Identicon.FromValue(value, size);
+
+            using (var stream = identicon.SaveAsPng())
+            {
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, (int)stream.Length);
+                var base64 = Convert.ToBase64String(buffer);
+                return Ok(base64);
+            }
+        }
+    }
+}
